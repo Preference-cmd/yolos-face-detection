556b30e5c7fb5099ec34eafc12f8fbd82e1f2b05
pref2rence <sq103832@outlook.com>
2025-12-01 16:34:10 +0800

    feat(scripts): 添加模型训练相关脚本
    feat(dataset): 实现FaceData数据集类的基本功能
    
    build: 在pyproject.toml中添加开发依赖

--- src/yolos_face/dataset.py
+++ src/yolos_face/dataset.py
@@ -1,12 +1,24 @@
 from torch.utils.data import Dataset
+from PIL import Image
+from transformers import AutoImageProcessor
 import torch
 
 class FaceData(Dataset):
-    def __init__(self) -> None:
+    def __init__(self, processor, transform = None) -> None:
         super().__init__()
+        self.processor = processor
+        self.transform = transform
+
+        self.image = Image.open(self.image_path)
+
+
+
+
+
+
 
     def __len__(self) -> int:
-        pass
+        return len(self.image)
 
 
     def __getitem__(self, index: int) -> torch.Tensor:
